d8de4356920222202a508536a4146f286d827f55
GennadiPyton <zikito777@mail.ru>
2022-04-13 09:01:13 +0300

    dfa

--- build/web/js/index.js
+++ build/web/js/index.js
@@ -1,38 +1,41 @@
-
 const menu_list_pages = document.getElementById("menu_list_pages");
 menu_list_pages.addEventListener("click",(e)=>{
-    toogleActiveMenu(e.target.id);
+    e.preventDefault();
+    toggleActiveMenu(e.target.id);
 })
 const menu_add = document.getElementById("menu_add");
 menu_add.addEventListener("click",(e)=>{
-    toogleActiveMenu(e.target.id);
+    e.preventDefault();
+    toggleActiveMenu(e.target.id);
 })
 
 const menu_profile = document.getElementById("menu_profile");
 menu_profile.addEventListener("click",(e)=>{
     e.preventDefault();
-    toogleActiveMenu(e.target.id);
+    toggleActiveMenu(e.target.id);
 })
 const menu_about = document.getElementById("menu_about");
 menu_about.addEventListener("click",(e)=>{
     e.preventDefault();
-    toogleActiveMenu(e.target.id);
-    hiddenMenu(e.target.id);
+    toggleActiveMenu(e.target.id);
+    toggleMenuLogin(e.target.id);
 })
 const menu_login = document.getElementById("menu_login");
 menu_login.addEventListener("click", (e) => {
     e.preventDefault();
-    hiddenMenu(e.target.id);
+    //toggleMenuLogin(e.target.id);
+    showLoginForm();
 })
 const menu_logout = document.getElementById("menu_logout");
 menu_logout.addEventListener("click",(e)=>{
     e.preventDefault();
-    hiddenMenu(e.target.id);
+    toggleMenuLogin(e.target.id);
+    sendLogout();
 })
 const info = document.getElementById("info");
 
 
-function toogleActiveMenu(selectedElementId){
+function toggleActiveMenu(selectedElementId){
     document.getElementById("info").innerHTML='';
     const listNavlinks = document.getElementsByClassName("nav-link");
     for(let i = 0; i < listNavlinks.length; i++){
@@ -50,19 +53,103 @@ function toogleActiveMenu(selectedElementId){
 }
 
 
-function hiddenMenu(elementId){
+function toggleMenuLogin(elementId){
     if(elementId === "menu_login"){
-        const menu_logout = document.getElementById("menu_logout");
-        menu_logout.classList.remove('d-none');
-        const menu_login = document.getElementById("menu_login")
-        menu_login.classList.add('d-none');
-        toogleActiveMenu("")
-        document.getElementById("info").innerHTML='Вы вошли';
+        hiddenMenuLogin()  
     }else if(elementId === "menu_logout"){
-        document.getElementById("menu_logout").classList.add('d-none');
-        document.getElementById("menu_login").classList.remove('d-none');
-        toogleActiveMenu("")
-        document.getElementById("info").innerHTML='Вы вышли';
+        showMenuLogin()
     }
    
 }
+function showMenuLogin(){
+    document.getElementById("menu_logout").classList.add('d-none');
+    document.getElementById("menu_login").classList.remove('d-none');
+    toggleActiveMenu("");
+    hiddenMenu();
+    document.getElementById("info").innerHTML='Вы вышли';
+}
+function hiddenMenuLogin(){
+    const menu_logout = document.getElementById("menu_logout");
+    menu_logout.classList.remove('d-none');
+    const menu_login = document.getElementById("menu_login")
+    menu_login.classList.add('d-none');
+    toggleActiveMenu("");
+    showMenu();
+    document.getElementById("info").innerHTML='Вы вошли';
+}
+function showMenu(){
+    document.getElementById('menu_add').classList.remove('d-none');
+    document.getElementById('menu_profile').classList.remove('d-none');
+}
+function hiddenMenu(){
+    document.getElementById('menu_add').classList.add('d-none');
+    document.getElementById('menu_profile').classList.add('d-none');
+}
+function showLoginForm(){
+    const content = document.getElementById('content');
+    content.innerHTML = `<div class="card border-primary mb-3 mx-auto" style="max-width: 30rem;">
+                            <h3 class="card-header text-center">АВТОРИЗАЦИЯ</h3>
+                            <div class="card-body">
+                              <div class="form-group">
+                                <label for="login" class="form-label mt-4">Логин</label>
+                                <input type="text" class="form-control" id="login" placeholder="Логин">
+                              </div>
+                              <div class="form-group">
+                                <label for="password" class="form-label mt-4">Пароль</label>
+                                <input type="password" class="form-control" id="password" placeholder="Пароль">
+                              </div>
+                            </div>
+                            <button id="btnLogin" type="submit" class="btn btn-primary m-3">ВОЙТИ</button>
+                        </div>`;
+    const btnLogin = document.getElementById('btnLogin');
+    btnLogin.addEventListener('click', (e)=>{
+        e.preventDefault();
+        sendCredential();
+    });
+}
+
+function sendCredential(){
+    const login = document.getElementById('login').value;
+    const password = document.getElementById('password').value;
+    const credential = {
+        "login": login,
+        "password": password
+    };
+    let promise = fetch('login',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset:utf8'
+        },
+        body: JSON.stringify(credential)
+    });
+    promise.then(response=> response.json())
+       .then(response =>{
+           if(response.auth){
+               document.getElementById('info').innerHTML = response.info;
+               showMenu();
+               hiddenMenuLogin();
+           }else{
+               hiddenMenu();
+               showMenuLogin();
+               document.getElementById('info').innerHTML = "Ошибка авторизации";
+           }
+       })
+       .catch(
+            document.getElementById('info').innerHTML = "Ошибка запроса"
+       )
+ }
+ function sendLogout(){
+     let promise = fetch('logout', {
+         method: 'GET',
+     });
+     promise.then(response => response.json())
+             .then(response => {
+                 if(!response.auth){
+                    hiddenMenu();
+                    showMenuLogin();
+                    document.getElementById('info').innerHTML = "Вы вышли";
+                    showLoginForm();
+                 }
+     })
+     
+ }
---
+++ src/java/servlets/LoginServlet.java
@@ -0,0 +1,137 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import entity.User;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.ejb.EJB;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.json.JsonReader;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import session.UserFacade;
+
+/**
+ *
+ * @author user
+ */
+@WebServlet(name = "LoginServlet", urlPatterns = {
+    "/login",
+    "/logout",
+    
+})
+public class LoginServlet extends HttpServlet {
+    @EJB private UserFacade userFacade;
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        JsonObjectBuilder job = Json.createObjectBuilder();
+        
+        String path = request.getServletPath();
+        switch (path) {
+            case "/login":
+                JsonReader jsonReader = Json.createReader(request.getReader());
+                JsonObject jo = jsonReader.readObject();
+                String login = jo.getString("login","");
+                String password = jo.getString("password","");
+                User authUser = userFacade.findByLogin(login);
+                if(authUser == null){
+                    job.add("info", "Нет такого пользователя");
+                    job.add("auth", false);
+                    try (PrintWriter out = response.getWriter()) {
+                        out.println(job.build().toString());
+                    }
+                    break;
+                }
+                if(!password.equals(authUser.getPasword())){
+                    job.add("info", "Не совпадает пароль");
+                    job.add("auth", false);
+                    try (PrintWriter out = response.getWriter()) {
+                        out.println(job.build().toString());
+                    }
+                    break;
+                }
+                HttpSession session = request.getSession(true);
+                session.setAttribute("authUser", authUser);
+                job.add("info", "Вы вошли как "+authUser.getLogin());
+                job.add("auth",true);
+                job.add("token", session.getId());
+                    try (PrintWriter out = response.getWriter()) {
+                        out.println(job.build().toString());
+                    }
+                break;
+            case "/logout":
+                session = request.getSession(false);
+                if(session != null){
+                    session.invalidate();
+                    job.add("info", "Вы вышли");
+                    job.add("auth", false);
+                    try (PrintWriter out = response.getWriter()) {
+                        out.println(job.build().toString());
+                    }
+                }
+                break;
+        }
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
